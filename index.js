d37d0fffe1fecff6928f15a46009f6491dd82c0e
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 17:50:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,8 @@ function createDriver(){
   let driverId = 0;
   return class {
     constructor(name){
-      this.name = name
-      this.id = ++driverId
+      this.name = name;
+      this.id = ++driverId;
     }
-    
-    
-  }
+  };
 }
