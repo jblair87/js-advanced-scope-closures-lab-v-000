882637b53e23b9c4d030c6748f8d64f1f3f5c670
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 18:02:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function produceDrivingRange (blockRange){
+
+   return function(start, end) {
+     let distance = parseInt(end) - parseInt(start);
+    if (blockRange < distance) 
+    return `${distance - blockRange} blocks out of range`;
+    else
+    return `within range by ${blockRange - distance}`;
+  };
+}
+
+function produceTipCalculator(percentage){
+   return function(riderFare) {
+  return riderFare * percentage;};
+}
+
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor(name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  };
+}
